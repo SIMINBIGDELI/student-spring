c8403d57b4cbe9bcabdd8f6d3c0d7f88d4cb16ee
siminbigdeli <siminbigdelii@gmail.com>
2022-09-19 19:44:53 -0400

    spring(@Component)

---
+++ src/main/java/com/keep/calm/java/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.keep.calm.java.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.keep.calm.java"})
+public class AppConfig {
+
+}
